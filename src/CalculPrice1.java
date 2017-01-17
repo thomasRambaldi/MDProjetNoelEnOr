d7a09ac1cf9c70fb1bc89f1fd1e569e98ab61419
Kevin <kevin@kevin>
2017-01-17 11:49:22 +0100

    maven

--- src/CalculPrice1.java
+++ src/CalculPrice1.java
@@ -4,7 +4,7 @@ import magasin.stock.Product;
 public class CalculPrice1 implements ICalculPrice{
 
 	@Override
-	public double calcul(Product product) {
+	public Double calcul(Product product) {
 		if(product.getQuantity() >= 200)
 			product.setPrice( product.getPrice() * 1.2 );
 		return product.getPrice();
--- src/CalculPrice2.java
+++ src/CalculPrice2.java
@@ -4,7 +4,7 @@ import magasin.stock.Product;
 public class CalculPrice2 implements ICalculPrice{
 
 	@Override
-	public double calcul(Product product) {
+	public Double calcul(Product product) {
 		if(product.getQuantity() <= 100 )
 			product.setPrice( product.getPrice() * 0.2 );
 		return product.getPrice();
